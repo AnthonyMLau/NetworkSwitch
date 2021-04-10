1d7dd7007bf5aa7893822768534702e8d318f033
anthony lau <anthony.lau2000@live.com>
2021-04-10 01:19:30 -0400

    main

---
+++ Switch.cpp
@@ -0,0 +1,9 @@
+#include <map>
+
+#include "EthernetFrame.h"
+#include "MACAddress.h"
+
+int main (int argc, char *argv[]){
+    // address : port #
+     std::map<long long int, int> addressTable;
+}
