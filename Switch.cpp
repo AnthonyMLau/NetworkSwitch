aa045b54189465d70a333330ae961cd3c099ac4e
anthony lau <anthony.lau2000@live.com>
2021-05-08 00:42:25 -0400

    adds some planning

--- Switch.cpp
+++ Switch.cpp
@@ -7,22 +7,44 @@
 #include "EthernetFrame.h"
 #include "MACAddress.h"
 
+#define MAXPORTS 10
+
+int addressTable[MAXPORTS][2];
+
 void *readInputSignals(void * arg){
+    //get destination and source addr
+    //get and allocate size
+    //write data into mem
+    //put in process buffer
 
 }
 
 void *writeOutputSignals(void * arg){
-    
+    //write bits to correct port/pin
 }
 
 void *processData(void * arg){
+    //compare data and find correct output port
+    //put in output buffer
+
+}
+
+void setup(){
+    // fill all values in addressTable to 0
+    for (int i = 0; i < MAXPORTS; i++){
+        for (int j = 0; j < 2; j++)
+        {
+            addressTable[i][j] = 0;
+        }
+    }
     
 }
 
 int main (int argc, char *argv[]){
     // address : port #
-    std::map<long long int, int> addressTable;
+    //std::map<long long int, int> addressTable;
 
+    setup();
 
     // todo: set thread affinity to separate cores for input and output
     // todo: multiple i/o for multiple signals 
